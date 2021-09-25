e5785badca89cbaf8a8a9c155775c91020cd920b
Aprea <apreak@gmail.com>
2021-09-25 20:06:51 +0700

    'first'

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+part of 'homelib.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            'This Project Start Here',
+            style: TextStyle(fontFamily: 'Oxygen'),
+          ),
+        ),
+      ),
+    );
+  }
+}
